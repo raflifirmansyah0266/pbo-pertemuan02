c35e2e9468feb0c35306b141fa5f588cb566bbc0
raflifirmansyah <rafifirman198@gmail.com>
2020-03-28 18:14:25 +0800

    Modifikasi Input Output

--- InputOutput.java
+++ InputOutput.java
@@ -1,12 +1,24 @@
-import java.until.Scanner;
+import java.util.Scanner;
 
 public class InputOutput{
  public static void main(String [] args){
   Scanner scanner = new Scanner(System.in);
   String nama;
+  int jumlahSks;
+  double Ipk;
+
   System.out.print("Masukkan nama anda: ");
-  nama = scanner.nextline();
+  nama = scanner.nextLine();
   System.out.println("Hai Senang Bertemu dengan mu, "+nama);
+
+  System.out.print("Masukkan jumlah SKS yang sudah ditempuh: ");
+  jumlahSks = scanner.nextInt()'
+
+  System.out.print("Masukkan IPK Sementara :");
+  IPK + scanner.nextDouble();
+
+  double jumlah = IPK * jumlahSks;
+  System.out.print("Total Nilai = "+jumlah);
 
  }
 }
